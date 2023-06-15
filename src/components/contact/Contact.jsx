b84cc86d9a805bc67c19b4fe6044bbe768be2c29
Janith Hathnagoda <hathnagodahwemjn.20@uom.lk>
2023-06-15 07:41:57 +0530

    General CSS styles added

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+  return(
+    <div>Contact</div>
+  )
+}
+
+
+export default Contact
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Portfolio.css'
+
+function Portfolio() {
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+export default Portfolio
